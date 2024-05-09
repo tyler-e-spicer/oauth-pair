b3eb4980b7da8ff29cdc532a5e1fa298a5671505
Colin Chauche <colin.chauche@gmail.com>
2024-05-08 19:00:45 -0600

    move useFetch to hooks folder

--- src/client/components/Login.jsx
+++ src/client/components/Login.jsx
@@ -1,27 +1,27 @@
 import React, { useState } from 'react';
 import './Login.css';
-import useFetch from './useFetch';
+import useFetch from '../hooks/useFetch';
 
 function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const { data, sendRequest } = useFetch();
-  const serverURL = 'http://localhost:3001'
+  const serverURL = 'http://localhost:3001';
 
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
       await sendRequest(serverURL + '/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        username,
-        password
-      })
-    });
-    console.log('response', data.message);
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username,
+          password,
+        }),
+      });
+      console.log('response', data.message);
     } catch (error) {
       console.log('Login error:', error);
     }
@@ -29,36 +29,40 @@ function Login() {
 
   const handleOAuth = (e) => {
     e.preventDefault();
-    console.log('OAuth Activate!')
-  }
+    console.log('OAuth Activate!');
+  };
 
-return (
-  <div className="login-container">
-    <h1>Login</h1>
-    <form onSubmit={handleLogin} className="form">
-      <label className="label">
-        Username:
-        <input
-          className="input"
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </label>
-      <label className="label">
-        Password:
-        <input
-          className="input"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </label>
-      <button className="button" type="submit">Login</button>
-    </form>
-    <button className="oauth-button" onClick={handleOAuth} type="submit">Login With Google!</button>
-  </div>
-);
+  return (
+    <div className="login-container">
+      <h1>Login</h1>
+      <form onSubmit={handleLogin} className="form">
+        <label className="label">
+          Username:
+          <input
+            className="input"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <label className="label">
+          Password:
+          <input
+            className="input"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button className="button" type="submit">
+          Login
+        </button>
+      </form>
+      <button className="oauth-button" onClick={handleOAuth} type="submit">
+        Login With Google!
+      </button>
+    </div>
+  );
 }
 
 export default Login;
